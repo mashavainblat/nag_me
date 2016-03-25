3886de213a170a8e851d79df738ab00c97b27f68
Masha Vainblat <MashaVainblat@Mashas-MacBook-Pro.local>
2016-03-25 13:27:53 -0400

    making form to add users

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,18 @@ var app = angular.module("NagMe", []);
 
 //need to access ajax service which is HTTP in angular
 app.controller("UsersController", ["$http", function($http){
+	var self = this;
+	this.users = [];
+
 	//make ajax request to server||users.js(controller)
 	//users.js(controller) makes request to mongo DB
 	//mongo DB accesses json data which feeds the UsersController
 	//wherever UsersController lives, json data will be console.logged
 	$http.get("/users/json").then(
 		//success
-		function(data){
-			console.log(data)
+		function(response){
+			self.users = response.data
+			console.log(response.data)
 		},
 		//failure
 		function(error){
@@ -17,3 +21,10 @@ app.controller("UsersController", ["$http", function($http){
 		}
 	)
 }]);
+
+app.controller("FormController", ["$http", function($http){
+	var self = this;
+	this.create = function(){
+		console.log(this)
+	}
+}])
