7280cec989ec14db5f216a9c74f47d597919ac75
Masha Vainblat <MashaVainblat@Mashas-MacBook-Pro.local>
2016-03-25 12:59:56 -0400

    making ajax request to DB and commenting

--- controllers/users.js
+++ controllers/users.js
@@ -2,19 +2,25 @@ var express = require("express");
 var router = express.Router();
 var User = require("../models/user.js");
 
-router.get("/", function(req, res){
+router.get("/json", function(req, res){
 	console.log("users controller")
-	res.send("Users");
-	var newUser = new User({
-		firstName: "Miriam",
-		lastName: "Vainblat",
-		email: "miriam@gmail.com",
-		password: "miriam",
-		phoneNumber: "+5162321815"
+	//ACCESSES DATABASE
+	User.find({}, function(err, users){
+		//feeds the view
+		res.json(users);
 	});
-	newUser.save(function(err, data){
-		console.log("Miriam is saved")
-	})
-})
+});
 
 module.exports = router;
+
+	// res.send("Users");
+	// var newUser = new User({
+	// 	firstName: "Miriam",
+	// 	lastName: "Vainblat",
+	// 	email: "miriam@gmail.com",
+	// 	password: "miriam",
+	// 	phoneNumber: "+5162321815"
+	// });
+	// newUser.save(function(err, data){
+	// 	console.log("Miriam is saved")
+	// })
--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,19 @@
 var app = angular.module("NagMe", []);
 
+//need to access ajax service which is HTTP in angular
+app.controller("UsersController", ["$http", function($http){
+	//make ajax request to server||users.js(controller)
+	//users.js(controller) makes request to mongo DB
+	//mongo DB accesses json data which feeds the UsersController
+	//wherever UsersController lives, json data will be console.logged
+	$http.get("/users/json").then(
+		//success
+		function(data){
+			console.log(data)
+		},
+		//failure
+		function(error){
+			console.log(error)
+		}
+	)
+}]);
